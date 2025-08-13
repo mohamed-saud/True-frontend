544edd8e7393bd9b65d1445f1febaaad6307c906
Mohamed Sayed <mohamedsayed@Mohameds-MacBook-Pro.local>
2025-08-14 01:20:22 +0300

    this commit will delet

--- components/homepage/categoriesBauner/CategoriesBuner.jsx
+++ components/homepage/categoriesBauner/CategoriesBuner.jsx
@@ -1,8 +1,12 @@
 import Category from './Category';
 function CategoriesBuner() {
   return (
-    <div className=' container m-auto'>
-      <div className='grid gap-5 grid '>
+    <div className=' container m-auto gap-4 grid'>
+      <div className='grid gap-4  grid-cols-[40%_1fr]'>
+        <Category />
+        <Category />
+      </div>
+      <div className='grid gap-4  grid-cols-[1fr_40%]'>
         <Category />
         <Category />
       </div>
--- components/homepage/categoriesBauner/Category.tsx
+++ components/homepage/categoriesBauner/Category.tsx
@@ -1,22 +1,29 @@
 import { Button } from '@/components/ui/button';
 import Heading from '@/components/ui/Heading';
 import SubTitle from '@/components/ui/SubTitle';
+import Title from '@/components/ui/Title';
+import { cn } from '@/lib/utils';
 import React from 'react';
 
-export default function Category() {
+export default function Category({ width }: { width: string }) {
   return (
-    <div className='bg-gray-100 p-5 '>
-      <div>
-        <SubTitle>Something completely new</SubTitle>
+    <a
+      href=''
+      className='bg-gray-100 h-[400px] p-8 rounded-xl flex items-start relative overflow-hidden'>
+      <div className=' min-w-[250px] grid gap-8 relative z-2 '>
+        <Title>Something completely new</Title>
         <Heading size='text-2xl'>Cases for Phone</Heading>
-        <Button>TO SHOP</Button>
+        <div>
+          <Button>TO SHOP</Button>
+        </div>
       </div>
       <div>
         <img
+          className=' absolute right-0 bottom-0 hover:scale-110 transition-all delay-75'
           src='https://woodmart.xtemos.com/accessories/wp-content/uploads/sites/7/2022/04/accessories-banner-2.jpg'
           alt=''
         />
       </div>
-    </div>
+    </a>
   );
 }
